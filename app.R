6d6e02bfefdf646c972872a2b18fc6f058000731
BursacPetar <bursac.petar3@gmail.com>
2023-02-04 14:43:59 +0100

    ...

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ library(readxl)
 source("ui.R")
 source("server.R")
 
-# runApp(host="0.0.0.0", port=8080)
+runApp(host="0.0.0.0", port=8080)
 # runApp(host="0.0.0.0", port=8080, launch.browser = TRUE)
 
 
